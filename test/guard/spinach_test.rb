3950d0d996ee33dce78abbb05b64560476030a05
Matt Burke <spraints@gmail.com>
2011-11-28 11:23:58 -0500

    Tests for Guard::Spinach#run_all

--- test/guard/spinach_test.rb
+++ test/guard/spinach_test.rb
@@ -20,4 +20,11 @@ describe Guard::Spinach do
       subject.run_on_change(['fake/path.feature', 'foo/bar.feature'])
     end
   end
+  describe "#run_all" do
+    it "fires run on a runner" do
+      Guard::Spinach::Runner.any_instance.expects(:system).with(
+        "spinach ")
+      subject.run_all
+    end
+  end
 end
