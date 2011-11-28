de01351aa2d47ff8279efb6969de43247f6cc64d
Matt Burke <spraints@gmail.com>
2011-11-28 11:23:35 -0500

    Finish the Guard::Spinach test.

--- test/guard/spinach_test.rb
+++ test/guard/spinach_test.rb
@@ -2,15 +2,22 @@ require_relative '../test_helper'
 
 describe Guard::Spinach do
   subject do
-    Guard::Spinach.new(data)
+    Guard::Spinach.new(data, options)
   end
   let(:paths) do
     ['fake/path.feature', 'foo/bar.feature']
+  end
+  let(:data) do
+    []
+  end
+  let(:options) do
+    {}
   end
   describe "#run_on_change" do
     it "fires run on a runner" do
       Guard::Spinach::Runner.any_instance.expects(:system).with(
         "spinach fake/path.feature foo/bar.feature")
+      subject.run_on_change(['fake/path.feature', 'foo/bar.feature'])
     end
   end
 end
